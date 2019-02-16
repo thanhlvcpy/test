2e6cc01fb178eeb4a51f9267978a4e189a5b2b47
thanhlvcpy <42794074+thanhlvcpy@users.noreply.github.com>
2019-02-16 21:26:19 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a[3];
+
+int main()
+{
+	int b[3];
+	for(int i=0;i<=2;i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+	for(int i=0;i<=2;i++)
+		cout<<b[i]<<" ";
+
+    return 0;
+}
---
+++ ocsenc2.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a,b,v;
+int main(){
+    cin>>a>>b>>v;
+    cout<<(v-a)/(a-b) + 1;
+}
